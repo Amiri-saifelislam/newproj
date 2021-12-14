05d0e0991167f6f225063ac5ed4d82f3e6f840d6
Anass IRIZI <irizi.anass.00@gmail.com>
2021-12-15 00:12:47 +0100

    filled the database with some data

--- database/generate_db.py
+++ database/generate_db.py
@@ -7,14 +7,14 @@ cur = db.cursor()
 cur.execute("""
     CREATE TABLE IF NOT EXISTS matiers(
         id INTEGER NOT NULL PRIMARY KEY,
-        nom TEXT
+        nom TEXT UNIQUE
     )
 """)
 
 cur.execute("""
     CREATE TABLE IF NOT EXISTS niveaux(
         id INTEGER NOT NULL PRIMARY KEY,
-        nom TEXT
+        nom TEXT UNIQUE
     )
 """)
 
@@ -26,7 +26,6 @@ cur.execute("""
         nom TEXT,
         prenom TEXT,
         date_of_birth TEXT,
-        niveau TEXT,
         FOREIGN KEY(niveau_id) REFERENCES niveaux(id)
     )
 """)
@@ -52,3 +51,77 @@ cur.execute("""
         FOREIGN KEY(etudiant_id) REFERENCES etudiants(id)
     )
 """)
+
+cur.execute(""" insert into matiers(nom) values("english") """)
+cur.execute(""" insert into matiers(nom) values("arabe") """)
+cur.execute(""" insert into matiers(nom) values("francais") """)
+cur.execute(""" insert into matiers(nom) values("education islamique") """)
+cur.execute(""" insert into matiers(nom) values("mathematique") """)
+
+cur.execute(""" insert into niveaux(nom) values("first grade") """)
+cur.execute(""" insert into niveaux(nom) values("second grade") """)
+cur.execute(""" insert into niveaux(nom) values("third grade") """)
+cur.execute(""" insert into niveaux(nom) values("fourth grade") """)
+cur.execute(""" insert into niveaux(nom) values("fifth grade") """)
+cur.execute(""" insert into niveaux(nom) values("sixt grade") """)
+
+cur.execute(""" insert into etudiants(niveau_id, cne, nom, prenom, date_of_birth)
+        select n.id,
+            "R130030312",
+            "IRIZI",
+            "Anass",
+            "2000-08-25"
+        from
+            niveaux n
+        where n.nom = "second grade"
+        """)
+
+cur.execute(""" insert into etudiants(niveau_id, cne, nom, prenom, date_of_birth)
+        select n.id,
+            "R000000000",
+            "AMIRI",
+            "Saif El Islam",
+            "2000-1-1"
+        from
+            niveaux n
+        where n.nom = "sixt grade"
+        """)
+
+cur.execute(""" insert into professeurs(matier_id, nom, prenom, date_of_birth)
+        select m.id,
+            "BEN LHOUSSAIN",
+            "Larbi",
+            "2000-1-1"
+        from
+            matiers m
+        where m.nom = "arabe"
+        """)
+
+cur.execute(""" insert into professeurs(matier_id, nom, prenom, date_of_birth)
+        select m.id,
+            "LHOUAT",
+            "Samira",
+            "2000-1-1"
+        from
+            matiers m
+        where m.nom = "francais"
+        """)
+
+cur.execute(""" insert into etudiants_professeurs (professeur_id, etudiant_id, note)
+        values (1, 1, 18.3)
+""")
+
+cur.execute(""" insert into etudiants_professeurs (professeur_id, etudiant_id, note)
+        values (1, 2, 10)
+""")
+
+cur.execute(""" insert into etudiants_professeurs (professeur_id, etudiant_id, note)
+        values (2, 1, 18.3)
+""")
+
+cur.execute(""" insert into etudiants_professeurs (professeur_id, etudiant_id, note)
+        values (2, 2, 10)
+""")
+
+cur.close()
+db.close()
